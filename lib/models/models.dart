9c9d76ec7b313ad4455383e824be63ab88c19d48
Adesh <Adeshpokhrel15@gmail.com>
2022-02-09 19:18:42 +0545

    flutter

---
+++ lib/models/models.dart
@@ -0,0 +1,31 @@
+class Wallpaper {
+  late int? id;
+  late int? width;
+  late int? height;
+  late String? url;
+  late String? photographer;
+  late int? photographerId;
+  late String? alt;
+
+  Wallpaper({
+    this.id,
+    this.width,
+    this.height,
+    this.url,
+    this.photographer,
+    this.photographerId,
+    this.alt,
+  });
+
+  factory Wallpaper.fromJson(Map<String, dynamic> json) {
+    return Wallpaper(
+      id: json['id'] ?? '',
+      width: json['width'] ?? '',
+      height: json['height'] ?? '',
+      url: json['url'] ?? '',
+      photographer: json['photographer'] ?? '',
+      photographerId: json['photographer_id'] ?? '',
+      alt: json['alt'] ?? '',
+    );
+  }
+}
